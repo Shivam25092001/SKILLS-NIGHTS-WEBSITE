5fc13fd60c692521937c1f67eaa283609c3d2f63
Shivam <ss056989@gmail.com>
2021-07-28 02:09:20 +0530

    Particle js file added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import Loader from './components/loader';
+import Main from './components/main.js'
+import Navbar from './components/navbar';
+import Event from './components/eventsPopup'
+import Archives from './components/archives'
+import Projects from './components/projects'
+import ParticleEffect from './components/particleeffect';
 
 function App() {
   return (
     <div className="App">
-      <h1>Skills Nights</h1>
+      <Navbar />
+      <Main />
+      <Loader />
+      <Event />
+      <Archives />
+      <Projects />
+      <ParticleEffect />
     </div>
   );
 }
