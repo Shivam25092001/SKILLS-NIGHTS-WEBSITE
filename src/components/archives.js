67f4e982eac692d908966cd647e375b47f96b454
ambujraj2001 <76392900+ambujraj2001@users.noreply.github.com>
2021-08-03 16:43:57 +0530

    Update archives.js

--- src/components/archives.js
+++ src/components/archives.js
@@ -2,3 +2,7 @@ document.writeln("Name : Srishti Pandey");
 document.writeln("Email : bt20cse068@iiitn.ac.in");
 document.writeln("Task : Archives");
 
+<<<<<<< Updated upstream
+=======
+qwertyfjhjgf
+>>>>>>> Stashed changes
